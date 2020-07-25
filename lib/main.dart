e78fa853c5cf7f4ca8dbfc8f660358f99cb1f7b9
개좆밥 <gaejotbab@gmail.com>
2020-07-25 20:29:09 +0900

    플래너 화면 다시 그리고 상태 추가하고 타이머 검은 화면 만듦

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
 
-void main() => runApp(EnergyChargedTimerApp());
+void main() => runApp(
+    ChangeNotifierProvider(
+      create: (context) => AppState(),
+      child: EnergyChargedTimerApp(),
+    )
+);
 
 class EnergyChargedTimerApp extends StatelessWidget {
   @override
@@ -80,22 +89,16 @@ class HomeScreenWidget extends StatefulWidget {
 
 enum TestEnum { a, b }
 
-class Goal {
-  final String name;
-  int _elapsedMilliseconds;
-
-  Goal(this.name) {
-  }
-}
-
 class _HomeScreenWidgetState extends State<HomeScreenWidget> {
-  List<Goal> _goals = [
-    Goal("국어"),
-    Goal("영어"),
-    Goal("수학"),
-    Goal("플러터"),
-    Goal("C 언어로 웹서버 만들기"),
-  ];
+  Timer _timer;
+
+  _HomeScreenWidgetState() {
+    _timer = new Timer.periodic(new Duration(milliseconds: 10), _onTimer);
+  }
+
+  void _onTimer(Timer timer) {
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -133,45 +136,55 @@ class _HomeScreenWidgetState extends State<HomeScreenWidget> {
               DefaultTextStyle(
                 style: DefaultTextStyle.of(context)
                     .style.apply(fontSizeFactor: 1.5),
-                child: ListView.builder(
-                  itemCount: _goals.length,
-                  itemBuilder: (context, i) =>
-                    Container(
-                      height: 50,
-                      child: Row(
-                        children: <Widget>[
-                          IconButton(
-                            icon: Icon(Icons.play_circle_filled),
-                            iconSize: 36,
-                            onPressed: () {
-                              Scaffold.of(context).showSnackBar(SnackBar(
-                                content: Text("버튼 누름"),
-                              ));
-                            },
-                          ),
-                          Expanded(
-                            child: Text(_goals[i].name,
-                              overflow: TextOverflow.ellipsis,
+                child: Consumer<AppState>(
+                  builder: (context, state, child) => ListView.builder(
+                    itemCount: state.goals.length,
+                    itemBuilder: (context, i) {
+                      var goal = state.goals[i];
+                      return Container(
+                        height: 50,
+                        child: Row(
+                          children: <Widget>[
+                            IconButton(
+                              color: goal.color,
+                              icon: Icon(Icons.play_circle_filled),
+                              iconSize: 36,
+                              onPressed: () {
+                                Navigator.push(
+                                    context,
+                                    MaterialPageRoute(builder: (context) =>
+                                        GoalTimerScreen()
+                                    )
+                                );
+                              },
                             ),
-                          ),
-                          SizedBox(width: 20),
-                          Text("00:00:00"),
-                          SizedBox(width: 10),
-                          PopupMenuButton<TestEnum>(
-                            itemBuilder: (context) => [
-                              PopupMenuItem<TestEnum>(
-                                value: TestEnum.a,
-                                child: Text("a랜다"),
+                            Expanded(
+                              child: Text(goal.name,
+                                overflow: TextOverflow.ellipsis,
                               ),
-                              PopupMenuItem<TestEnum>(
-                                value: TestEnum.b,
-                                child: Text("b라더라"),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
+                            ),
+                            SizedBox(width: 20),
+                            Text("${goal.stopwatch.elapsed
+                                .inSeconds}"),
+                            SizedBox(width: 10),
+                            PopupMenuButton<TestEnum>(
+                              itemBuilder: (context) =>
+                              [
+                                PopupMenuItem<TestEnum>(
+                                  value: TestEnum.a,
+                                  child: Text("a랜다"),
+                                ),
+                                PopupMenuItem<TestEnum>(
+                                  value: TestEnum.b,
+                                  child: Text("b라더라"),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      );
+                    }
+                  ),
                 )
               ),
           ),
@@ -179,6 +192,92 @@ class _HomeScreenWidgetState extends State<HomeScreenWidget> {
       ],
     );
   }
+}
+
+class GoalTimerScreen extends StatefulWidget {
+  @override
+  _GoalTimerScreenState createState() => _GoalTimerScreenState();
+}
+
+class _GoalTimerScreenState extends State<GoalTimerScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.black,
+        child: Builder(
+          builder: (context) => DefaultTextStyle(
+            style: DefaultTextStyle.of(context).style.apply(color: Colors.white),
+            child: Column(
+              children: <Widget>[
+                SizedBox(height: 72),
+                Text("총 공부시간"),
+                SizedBox(height: 8),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text("00:00:00", textScaleFactor: 4),
+                    SizedBox(width: 8),
+                    IconButton(
+                      icon: Icon(Icons.pause_circle_filled),
+                      iconSize: 64,
+                      color: Colors.white,
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ],
+                ),
+                SizedBox(height: 48),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Text("국어"),
+                          SizedBox(height: 4),
+                          Text("00:00:00", textScaleFactor: 1.5),
+                        ],
+                      ),
+                    ),
+                    Expanded(
+                      child: Column(
+                        children: [
+                          Text("현재 집중시간"),
+                          SizedBox(height: 4),
+                          Text("00:00:00", textScaleFactor: 1.5),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Goal {
+  final String name;
+  final Color color;
+  Stopwatch stopwatch = Stopwatch();
+
+  Goal(this.name, this.color) {
+  }
+}
+
+class AppState extends ChangeNotifier {
+  final List<Goal> goals = [
+    Goal("국어", Colors.red),
+    Goal("수학", Colors.purple),
+    Goal("영어", Colors.cyan),
+    Goal("개발", Colors.black26),
+  ];
+
+  int totalElapsedTime = 0;
 }
 
 class StatisticsScreenWidget extends StatefulWidget {
@@ -242,78 +341,145 @@ class _PlannerScreenWidgetState extends State<PlannerScreenWidget> {
             ),
           ),
         ),
-        Expanded(
+        SizedBox(
+          height: 6,
           child: Container(
             color: Colors.black12,
-            child: Table(
-              children: [
-                TableRow(
-                  children: [
-                    TableCell(
-                      verticalAlignment: TableCellVerticalAlignment.fill,
-                      child: Container(
-                        color: Colors.white,
-                        child: FlatButton(
-                          child: Text("오늘의 한마디"),
-                          onPressed: () {
-                            Scaffold.of(context).showSnackBar(
-                              SnackBar(content: Text("오늘의 한 마디 누름"))
-                            );
-                          },
+          ),
+        ),
+        Row(
+          children: [
+            FlatButton(
+              child: Text("오늘의 한마디"),
+              onPressed: () {
+                Scaffold.of(context).showSnackBar(
+                    SnackBar(content: Text("오늘의 한마디 버튼 누름"))
+                );
+              },
+            ),
+            Expanded(
+              child: Column(
+                children: <Widget>[
+                  FlatButton(
+                    child: Text("D-day"),
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: LinearProgressIndicator(
+                          value: 0.3,
                         ),
                       ),
-                    ),
-                    Container(
-                      color: Colors.white,
-                      child: Column(
-                        children: <Widget>[
-                          FlatButton(
-                            child: Text("D-Day"),
-                            onPressed: () {
-                              Scaffold.of(context).showSnackBar(
-                                SnackBar(content: Text("D-Day 누름"))
-                              );
-                            },
-                          ),
-                          Row(
-                            children: <Widget>[
-                              SizedBox(width: 4),
-                              Expanded(
-                                child: SizedBox(
-                                  height: 16,
-                                  child: LinearProgressIndicator(
-                                    value: 0.3,
-                                    backgroundColor: Colors.black12,
-                                    valueColor: AlwaysStoppedAnimation(Colors.black54),
-                                  ),
-                                ),
-                              ),
-                              SizedBox(width: 4),
-                              Text("0.0"),
-                              SizedBox(width: 4),
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-                TableRow(
-                  children: [
-                    SizedBox(
-                      child: Container(
-                          color: Colors.white,
-                          child: Text("과목 목록")
-                      ),
-                    ),
-                    Container(
-                        color: Colors.white,
-                        child: Text("시간표")
-                    ),
-                  ],
-                ),
-              ]
+                      SizedBox(width: 4),
+                      Text("0.0"),
+                      SizedBox(width: 4),
+                    ],
+                  ),
+                ],
+              ),
             ),
+          ]
+        ),
+        SizedBox(
+          height: 6,
+          child: Container(
+            color: Colors.black12,
+          ),
+        ),
+        Expanded(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: ListView(
+                  children: <Widget>[
+                    Column(
+                      children: <Widget>[
+                        Row(
+                          children: [
+                            Container(
+                              width: 8,
+                              height: 36,
+                              color: Colors.red,
+                              child: Text(""),
+                            ),
+                            SizedBox(width: 8),
+                            Text("국어", textScaleFactor: 1.2,),
+                          ],
+                        ),
+                        Row(
+                          children: <Widget>[
+                            SizedBox(width: 16),
+                            Text(
+                              "00:01:13",
+                              style: TextStyle(color: Theme.of(context).primaryColor),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                    Column(
+                      children: <Widget>[
+                        Row(
+                          children: [
+                            Container(
+                              width: 8,
+                              height: 36,
+                              color: Colors.green,
+                              child: Text(""),
+                            ),
+                            SizedBox(width: 8),
+                            Text("영어", textScaleFactor: 1.2,),
+                          ],
+                        ),
+                        Row(
+                          children: <Widget>[
+                            SizedBox(width: 16),
+                            Text(
+                              "00:01:13",
+                              style: TextStyle(color: Theme.of(context).primaryColor),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Column(
+                children: [
+                  Row(
+                    children: [
+                      Text("5시"),
+                      Container(
+                        width: 1,
+                        color: Colors.black12,
+                      ),
+                      SizedBox(
+                        width: 30,
+                        height: 30,
+                      ),
+                      SizedBox(
+                        width: 30,
+                        height: 30,
+                      ),
+                      SizedBox(width: 30, height: 30),
+                      SizedBox(
+                        width: 30,
+                        height: 30,
+                      ),
+                      SizedBox(
+                        width: 30,
+                        height: 30,
+                      ),
+                      SizedBox(
+                        width: 30,
+                        height: 30,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
           ),
         )
       ]
